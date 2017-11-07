d29cf1fe47ce9b22080f7bbde99f449e12ee5289
wann_he <hwn580231@gmail.com>
2017-11-08 00:29:37 +0800

    no msg

---
+++ vue_app_demo/src/components/page/Pos.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pos">
+      <!-- Hello WannPos Demo. -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos'
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
