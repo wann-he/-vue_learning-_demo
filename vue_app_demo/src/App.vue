85cf0fd9e96875899dbe0097f1da3fe53a271a50
wann_he <hwn580231@gmail.com>
2017-11-10 00:22:44 +0800

    no msg

--- vue_app_demo/src/App.vue
+++ vue_app_demo/src/App.vue
@@ -3,33 +3,32 @@
     <left-nav></left-nav>
 
     <div class="main">
-    <router-view/>
-
+      <router-view/>
     </div>
     
   </div>
 </template>
 
 <script>
-import leftNav from '@/components/common/leftNav'
+import leftNav from "@/components/common/leftNav";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     leftNav
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 0px;
 }
-.main{
+.main {
   float: left;
   width: 95%;
   background-color: #eff2f7;
